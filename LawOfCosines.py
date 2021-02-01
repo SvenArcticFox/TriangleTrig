4b6230003cf8a02b8f09af5ae344498a3dc7a8b9
SvenArcticFox <74692162+SvenArcticFox@users.noreply.github.com>
2021-02-01 08:42:43 -0500

    Law of Cosines can calculate angle

--- LawOfCosines.py
+++ LawOfCosines.py
@@ -37,6 +37,50 @@ def calculateSide():
     sideC = math.sqrt(sideC)
     print("Side C = " + str(sideC))
 
+def calculateAngle():
+    validFloat = False
+    while not validFloat:
+        sideA = input("Enter a number for side a\n")
+        if sideA.isnumeric():
+            sideA = float(sideA)
+            validFloat = True
+        else:
+            print("Input a valid real number")
+
+    validFloat = False
+    while not validFloat:
+        sideB = input("Enter a number side b\n")
+        if sideB.isnumeric():
+            sideB = float(sideB)
+            validFloat = True
+        else:
+            print("Input a valid real number")
+
+    validFloat = False
+    while not validFloat:
+        sideC = input("Enter a number for side c\n")
+        if sideC.isnumeric():
+            sideC = float(sideC)
+            validFloat = True
+        else:
+            print("Input a valid real number")
+
+    sideC = math.pow(sideC, 2)
+    d = math.pow(sideA, 2) + math.pow(sideB, 2)
+    e = -2 * sideA * sideB
+    sideC -= d
+    e = sideC / e
+
+    if e < 0:
+        alpha = AngleConversions.convertDegrees(math.acos(abs(e)))
+        print("Alpha = " + str(alpha))
+        angleC = 180 - alpha
+        print("Angle C = " + str(angleC))
+    else:
+        angleC = AngleConversions.convertDegrees(math.acos(abs(e)))
+        print("Alpha = " + str(angleC))
+        print("Angle C = " + str(angleC))
+
 
 print("Law of Cosines")
 validInt = False
@@ -47,7 +91,7 @@ while not validInt:
         decision = int(decision)
         if decision == 1:
             calculateSide()
-        #elif decision == 2:
-            #calculateAngle()
+        elif decision == 2:
+            calculateAngle()
     else:
         print("Please Enter 1 or 2")
